281cca18fac6bcc454339528481b3cdce307655b
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-22 22:11:57 +0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+
+  navToggle.classList.toggle("active");
+
+  navMenu.classList.toggle("active");
+
+});
