02ab2dea3a1933af4e184cccb62e88b6dd3c8a9d
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-22 22:08:33 +0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-  navToggle.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
