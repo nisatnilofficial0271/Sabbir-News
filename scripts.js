d1ce0595f0e9bd6f50972ae7eaf3bee91a1d0c6c
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-24 22:08:40 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,9 @@ navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 
 });
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarMenu = document.querySelector('.navbar-nav');
+
+navbarToggler.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
